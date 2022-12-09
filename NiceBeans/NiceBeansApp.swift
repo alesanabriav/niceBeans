10993f436560d6e636398858a372ed553ea096b0
Alejandro Sanabria <alesanabriav@gmail.com>
2022-12-09 11:57:11 -0500

    Initial Commit

---
+++ NiceBeans/NiceBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceBeansApp.swift
+//  NiceBeans
+//
+//  Created by Alejandro Sanabria on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NiceBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
